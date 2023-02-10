857b752da36f8897dd91a9e3de0843d04cb22b76
Grendel <jcg@grendel.world>
2023-02-09 19:54:10 -0600

    Use properties to alias internal Label.text fields

--- components/Thing/Thing.gd
+++ components/Thing/Thing.gd
@@ -1,13 +1,26 @@
 extends HBoxContainer
 
+export(String) var key: String setget set_key, get_key
+export(String) var value: String setget set_value, get_value
 
-export(String) var key: String
-export(String) var value: String
+
+func set_key(new_key: String) -> void:
+	$"%Key".text = new_key
+
+
+func get_key() -> String:
+	return $"%Key".text
+
+
+func set_value(new_value: String) -> void:
+	$"%Value".text = new_value
+
+
+func get_value() -> String:
+	return $"%Value".text
 
 
 func _ready() -> void:
-	$"%Key".text = key
-	$"%Value".text = value
 	$"%DeleteButton".connect("pressed", self, "_on_delete_pressed")
 
 
